fd15bccd7f5d1026015927b93659b85b2c47b25f
figlo <miro@figlar.sk>
2022-01-16 22:11:54 +0100

    Initial commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,24 +7,39 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+
 fun main() {
     var file: File
     var image: BufferedImage
     var newFile: File
-    val newFont = Font("Arial", Font.BOLD, 250)
+
+    val newFont = Font("arial", Font.BOLD, 550)
+
     CardSuit.values().forEach { cardSuit ->
-        file = File("C:\\Users\\Miro\\playing_cards\\${cardSuit.name.lowercase()}_final.png")
+        file = File("C:\\Users\\Miro\\playing_cards\\${cardSuit.name.lowercase()}_sablona.png")
         CardFace.values().forEach { cardFace ->
             image = ImageIO.read(file)
             with(image.graphics) {
                 font = newFont
                 color = when (cardSuit) {
-                    SPADES, CLUBS -> Color.BLACK
-                    HEARTS, DIAMONDS -> Color.RED
+                    SPADES -> Color.BLACK
+                    CLUBS -> Color.decode("#35654d")
+                    HEARTS -> Color.decode("#ff3322")
+                    DIAMONDS -> Color.decode("#0051d8")
                 }
-                drawString(cardFace.value, 30, 220)
+                val value = cardFace.value
+                val x = when(value) {
+                    "10" -> {
+                        drawString("1", -20, 650)
+                        drawString("0", 200, 650)
+                        return@with
+                    }
+                    "K", "Q", "A" -> 60
+                    else -> 170
+                }
+                drawString(value, x, 650)
             }
-            newFile = File("C:\\Users\\Miro\\playing_cards\\output\\${cardFace.abbr}${cardSuit.abbr}.png")
+            newFile = File("C:\\Users\\Miro\\playing_cards\\output\\card_${cardFace.abbr}${cardSuit.abbr}.png")
             ImageIO.write(image, "png", newFile)
         }
     }
