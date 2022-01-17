0182b2168d8b61fde126fa056f9e752fa947e4c5
figlo <miro@figlar.sk>
2022-01-17 20:10:35 +0100

    new cards based on Card Characters font

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,44 +1,38 @@
 package PlayingCardsGenerator
 
-import PlayingCardsGenerator.CardSuit.*
 import java.awt.Color
 import java.awt.Font
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-
 fun main() {
     var file: File
     var image: BufferedImage
     var newFile: File
 
-    val newFont = Font("arial", Font.BOLD, 550)
+    val suitFont = Font("card characters", Font.PLAIN, 170)
+    val faceFont = Font("card characters", Font.PLAIN, 540)
 
     CardSuit.values().forEach { cardSuit ->
-        file = File("C:\\Users\\Miro\\playing_cards\\${cardSuit.name.lowercase()}_sablona.png")
+        val suitColor = Color.decode(cardSuit.hexColor)
+
         CardFace.values().forEach { cardFace ->
+            file = File("C:\\Users\\Miro\\playing_cards\\sablona.png")
             image = ImageIO.read(file)
+
             with(image.graphics) {
-                font = newFont
-                color = when (cardSuit) {
-                    SPADES -> Color.BLACK
-                    CLUBS -> Color.decode("#35654d")
-                    HEARTS -> Color.decode("#ff3322")
-                    DIAMONDS -> Color.decode("#0051d8")
-                }
-                val value = cardFace.value
-                val x = when(value) {
-                    "10" -> {
-                        drawString("1", -20, 650)
-                        drawString("0", 200, 650)
-                        return@with
-                    }
-                    "K", "Q", "A" -> 60
-                    else -> 170
-                }
-                drawString(value, x, 650)
+                font = suitFont
+                color = suitColor
+                drawString(cardSuit.value, 30, 180)
             }
+
+            with(image.graphics) {
+                font = faceFont
+                color = suitColor
+                drawString(cardFace.value, 130, 650)
+            }
+
             newFile = File("C:\\Users\\Miro\\playing_cards\\output\\card_${cardFace.abbr}${cardSuit.abbr}.png")
             ImageIO.write(image, "png", newFile)
         }
